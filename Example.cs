5d3bb2ce7508238fa4f4386204e232d043ff2a13
wesbanq <wesbanq@proton.me>
2026-02-03 18:06:44 +0200

    help text

--- Example.cs
+++ Example.cs
@@ -2,6 +2,7 @@
 
 internal enum MyOption
 {
+    None = 0,
     [Option("a", "Receive a single value.", OptionType.SingleValue)]
     Option1,
     [Option("b", "Receive a multiple values.", OptionType.MultipleValues)]
@@ -12,14 +13,20 @@ internal enum MyOption
 
 internal enum MyAction
 {
+    None = 0,
+    [Command<MyOption>("action1", "Print first two options.")]
     Action1,
+    [Command<MyOption>("action2", "Print third option and its aliases.")]
     Action2,
 }
 
 internal class MyArgs : NuArgs<MyOption, MyAction>
 {
+    [Alias<MyOption>([MyOption.Option1, MyOption.Option2])]
     public bool? Field1;
+    [Alias<MyOption>([MyOption.Option3])]
     public string? Field2;
+    [Alias<MyOption>([MyOption.Option3], (s) => s.ToList())]
     public int[]? Field3;
 }
 
